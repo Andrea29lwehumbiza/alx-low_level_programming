12861d4e3ec43a94e22046e3e12d330ce6384e8b
Andrea29lwehumbiza <andrealwehumbiza@gmail.com>
2023-07-13 17:52:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	write(2,
-8	"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+8	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }
