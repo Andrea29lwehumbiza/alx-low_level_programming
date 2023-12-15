3ba63eb304889da3ed04a16280ee2eca58ab9ac6
Andrea29lwehumbiza <andrealwehumbiza@gmail.com>
2023-12-15 15:56:31 -0800

    dynamic libraries

--- 0x18-dynamic_libraries/functions/3-puts.c
+++ 0x18-dynamic_libraries/functions/3-puts.c
@@ -4,7 +4,15 @@
 #include <ctype.h>
 #include <string.h>
 
+/**
+ * _puts - Prints a string to the standard output followed by a newline.
+ * @s: A pointer to the string to be printed.
+ *
+ * Description: This function takes a pointer to a string and prints each
+ * character of the string to the standard output using the `puts` function.
+ * A newline character is appended after the string is printed
+ */
 void _puts(char *s)
 {
-puts(s);
+	puts(s);
 }
