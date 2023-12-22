a277034c953e25c9aaf35e64c756f68d31bc782e
Andrea29lwehumbiza <andrealwehumbiza@gmail.com>
2023-12-22 11:31:02 -0800

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,30 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * key_index - computes the index for a given key
- * @key: the key (string) to hash
- * @size: size of the hash table
+ * key_index - get index for key given
+ * @key: key (string) to hash
+ * @size: size of hash table
  *
- * Return: index of 'key' using djb2 hash function
+ * Return: index of 'key' using djb2
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	/*
-	 * hash_djb2 - hash function using the djb2 algorithm
-	 * @str: the string to hash
-	 *
-	 * Return: the hash value
-	 */
-	unsigned long int hash_djb2(const unsigned char *str)
-	{
-		unsigned long int hash = 5381;
-		int c;
-
-		while ((c = *str++))
-			hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
-		return (hash);
-	}
-
 	return (hash_djb2(key) % size);
 }
